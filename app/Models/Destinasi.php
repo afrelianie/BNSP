f9caed280fb697f632e239d8708b2b328d2b119c
fauziepangestu <fauziepangestu7@gmail.com>
2023-05-31 00:03:02 +0700

    relasi table pesanan

--- app/Models/Destinasi.php
+++ app/Models/Destinasi.php
@@ -27,4 +27,8 @@ class Destinasi extends Model
             return $this->hasMany(DetailModel::class, 'id');
         }
     
+    public function pesanan()
+        {
+            return $this->hasMany(PesananModel::class, 'id');
+        }
 }
